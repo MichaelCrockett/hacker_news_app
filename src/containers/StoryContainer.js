f6ae51717391e24dfbfa7fa8a0b1920cdb87c6fb
Michael Crockett <mike.crockett76@gmail.com>
2019-10-30 13:49:37 +0000

    Data showing in devtools

--- src/containers/StoryContainer.js
+++ src/containers/StoryContainer.js
@@ -4,6 +4,26 @@ import StoryList from '../components/StoryList';
 
 class StoryContainer extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      topStories: [],
+      story: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://hacker-news.firebaseio.com/v0/topstories.json")
+      .then(res => res.json())
+      .then(data => this.setState({ topStories: data }))
+      .catch(console.error);
+
+    fetch("https://hacker-news.firebaseio.com/v0/item/{storyId}.json")
+    .then(res => res.json())
+    .then(data => this.setState({ story: data }))
+    .catch(console.error);
+  }
 
   render () {
     return (
